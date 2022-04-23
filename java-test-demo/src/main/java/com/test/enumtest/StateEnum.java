832f65f476ab80fe06cc775565f73d72cca5b8d1
gz <gaozhao0224@163.com>
2022-04-23 17:54:40 +0800

    1

---
+++ java-test-demo/src/main/java/com/test/enumtest/StateEnum.java
@@ -0,0 +1,38 @@
+package com.test.enumtest;
+
+
+public enum StateEnum {
+
+
+    CG("1","成功"),
+    SB("2","失败"),
+    DD("3","待定"),;
+
+    private final String key ;
+    private final String value ;
+
+
+    StateEnum(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public String key() {
+        return this.key;
+    }
+
+    public String value() {
+        return this.value;
+    }
+
+    public String getValue(String type) {
+        StateEnum[] stateEnums = values();
+        for (StateEnum stateEnum : stateEnums) {
+            if (stateEnum.key().equals(key)) {
+                return stateEnum.value();
+            }
+        }
+        return null;
+    }
+
+}
